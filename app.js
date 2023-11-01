df0b9e7d9864773bc3ac42da1f65dac5572557db
Neira-Facundo <112208137+Neira-Facundo@users.noreply.github.com>
2023-11-01 20:20:10 -0300

    creacion de ruta pasajero

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { paisesRouter } from "./paises.js";
 import { avionRouter } from "./avion.js";
 import {boletoRouter} from "./boleto.js"
 import { vueloRouter } from "./vuelo.js";
+import { pasajeroRouter } from "./pasajero.js";
 
 
 // Creo aplicacion express
@@ -12,9 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/paises", paisesRouter);
-app.use("/avion",avionRouter)
+app.use("/avion", avionRouter)
 app.use("/boleto", boletoRouter)
 app.use("/vuelo", vueloRouter)
+app.use("/pasajero", pasajeroRouter)
 
 // Registrar metodo GET en ruta raiz ('/')
 app.get("/", (req, res) => {
---
+++ pasajero.js
@@ -0,0 +1,58 @@
+import express from "express";
+import { db } from "./db.js";
+import { body, param, query, validationResult } from "express-validator";
+
+//GET/pasajero
+export const pasajeroRouter = express.Router();
+
+pasajeroRouter.get("/", async (req, res) => {
+  const [rows, fields] = await db.execute("SELECT * FROM pasajero");
+  res.send(rows);
+});
+
+
+//POST/pasajero
+pasajeroRouter.post(      //En body, se indica el campo, luego el tipo de dato, y al ultimo la longitud
+    "/",
+    body("nombre").isString().isLength({ min: 1, max: 50 }),
+    body("documento").isString().isLength({ min: 1, max: 50 }),
+    body("telefono").isInt().isLength({ min: 1, max: 11 }),
+    body("nacionalidad").isString().isLength({ min: 1, max: 20 }),
+    body("correo").isString().isLength({ min: 1, max: 50 }),
+    body("direccion").isString().isLength({ min: 1, max: 50 }),
+
+
+    async (req, res) => {
+      const validacion = validationResult(req);
+      if (!validacion.isEmpty()) {
+        res.status(400).send({ errors: validacion.array() });
+        return;
+      }
+
+      const { nombre, documento, telefono, nacionalidad, correo, direccion  } = req.body;   //se crea una constante con los parametros de la tabla
+      const [rows] = await db.execute(   //se agrega el pasajero a la fila
+        "INSERT INTO pasajero (nombre, documento, telefono, nacionalidad, correo, direccion) VALUES (:nombre, :documento, :telefono, :nacionalidad, :correo, :direccion)",  
+        { nombre, documento, telefono, nacionalidad, correo, direccion   }
+      );
+      res.status(201).send({ nombre, documento, telefono, nacionalidad, correo, direccion, id: rows.insertId });
+    }
+  );
+
+
+
+
+
+/* Como se agrega,  POST 
+-Body
+
+{
+  "nombre": ".........",
+  "documento": "..........",
+  "telefono": "..........",
+  "nacionalidad": "........",
+  "correo": "..............",
+  "direccion": "............."
+}
+
+*/
+
