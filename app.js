809c016bfad40e040706375a62e3bbdc5892c2d9
YisusK3 <jesuskaram3@gmail.com>
2023-11-01 20:35:22 -0300

    Se crea ruta detalle
    Lo hizo Mateo pero no le deja subirlo

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import { paisesRouter } from "./paises.js";
 import { avionRouter } from "./avion.js";
 import {boletoRouter} from "./boleto.js"
 import { vueloRouter } from "./vuelo.js";
+import { detallevueloRouter } from "./detalle.js";
+import { pasajeroRouter } from "./pasajero.js";
 
 
 // Creo aplicacion express
@@ -12,9 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/paises", paisesRouter);
-app.use("/avion",avionRouter)
-app.use("/boleto", boletoRouter)
-app.use("/vuelo", vueloRouter)
+app.use("/avion",avionRouter);
+app.use("/boleto", boletoRouter);
+app.use("/vuelo", vueloRouter);
+app.use("/detallevuelo", detallevueloRouter);
+app.use("/pasajero", pasajeroRouter);
 
 // Registrar metodo GET en ruta raiz ('/')
 app.get("/", (req, res) => {
