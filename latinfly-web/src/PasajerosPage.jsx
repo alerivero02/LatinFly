6800634260692434c9fb271d0cdfe9e95890e276
Alexander Rivero <al.rivero2021@gmail.com>
2023-11-17 18:29:56 -0300

    link para pasajeros

--- latinfly-web/src/PasajerosPage.jsx
+++ latinfly-web/src/PasajerosPage.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { Link,useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
