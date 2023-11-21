be85a6a13a05e2f883eba6dac2cf93bee2384b5b
Neira-Facundo <112208137+Neira-Facundo@users.noreply.github.com>
2023-11-21 00:45:35 -0300

    post
    correccion en API ,detalle.js

--- api-aerolinea/detalle.js
+++ api-aerolinea/detalle.js
@@ -8,6 +8,30 @@ export const detallesvuelosRouter = express.Router();
 //http://localhost:3000/detallesvuelos/:id
 //Trae la informacion de vuelo por el id
 
+
+
+detallesvuelosRouter.post(      //En body, se indica el campo, luego el tipo de dato, y al ultimo la longitud.
+    "/",
+
+    async (req, res) => {
+      const validacion = validationResult(req);
+      if (!validacion.isEmpty()) {
+        res.status(400).send({ errors: validacion.array() });
+        return;
+      }
+
+      const { idBoleto, idPasajero } = req.body;   //se crea una constante para ingresar los datos
+      const [rows] = await db.execute(   //se agrega el pasajeros a la fila
+      //Se le insertan los valores a los campos indicados
+        "INSERT INTO detallesvuelos (idBoleto, idPasajero) VALUES (:idBoleto, :idPasajero)",  
+        { idBoleto:idBoleto, idPasajero:idPasajero  }
+      );
+      res.status(201).send({ id: rows.insertId, idBoleto, idPasajero  });
+    }
+  );
+
+
+
 detallesvuelosRouter.get("/:id", param("id").isInt({ min: 1 }), async (req, res) => {
   const validacion = validationResult(req);
   if (!validacion.isEmpty()) {  
@@ -24,4 +48,7 @@ detallesvuelosRouter.get("/:id", param("id").isInt({ min: 1 }), async (req, res)
   } else {
     res.status(404).send({ mensaje: "Detalle de vuelo no encontrado" });
   }
+
+
+
 })
